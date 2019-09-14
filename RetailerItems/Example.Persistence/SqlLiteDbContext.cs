80dfe6f134d9fd89ba2b7102534515a1c0b2b3d4
zubair <zubair@haefelesoftware.com>
2019-09-14 21:26:28 +0200

    added json config for DB. Added DB context

--- RetailerItems/Example.Persistence/SqlLiteDbContext.cs
+++ RetailerItems/Example.Persistence/SqlLiteDbContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Example.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Example.Persistence
 {
     public class SqlLiteDbContext : DbContext
     {
+        public SqlLiteDbContext(DbContextOptions<SqlLiteDbContext> options)
+            : base(options)
+        {
+        }
         
+        public DbSet<Retailer> Retailers { get; set; }
+        public DbSet<Item> Items { get; set; }
     }
 }
